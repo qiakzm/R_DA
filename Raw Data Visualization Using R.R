12de88e808cf552b94f8ed1c27b4e1ebce0e78b6
qiakzm <hjune98@gmail.com>
2023-04-06 16:16:09 +0900

    Update on Subscription on code.

--- Raw Data Visualization Using R.R
+++ Raw Data Visualization Using R.R
@@ -6,6 +6,8 @@ library(cowplot)
 library(forecast)
 TSLA = read.csv("TSLA.csv")
 TSLA$Date = as.Date(TSLA$Date)
+
+#1. 테슬라 주식의 날짜별 가격들 시각화. (거래량은 단위가 다르기 때문에 제외)
 TSLA %>%
   select(-Volume) %>%
   melt(id.vars = c("Date")) %>%
@@ -19,6 +21,8 @@ TSLA %>%
        theme_bw()+
        theme(text = element_text(size = 15, face = "bold"),
              legend.position = c(0.2,0.8))
+
+#2. 테슬라 주식의 거래량(Volume) 시각화.
  TSLA %>%
        select(Date, Volume) %>%
        ggplot() +
@@ -30,6 +34,8 @@ TSLA %>%
        labs(col = "Types of Price")+
        theme_bw() +
        theme(text = element_text(size = 15, face = "bold"))
+ 
+#3. 1번과 2번을 동시에 표현
 A = TSLA %>%
        select(-Volume) %>%
        melt(id.vars = c("Date")) %>%
